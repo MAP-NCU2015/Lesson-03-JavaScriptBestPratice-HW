f03b1e3a56073f33dd5bc0372ee7a8192ab806d2
Shark <sharknevercries@gmail.com>
2015-10-12 00:18:19 +0800

    Deal with this and object properties.

--- homework/content.js
+++ homework/content.js
@@ -1,37 +1,49 @@
 'use strict';
 
-(function() {
-  var _wrapper = document.querySelector('#note-content-wrapper');
+(function (exports) {
 
-  function start() {
-    window.addEventListener('note-open', function(event) {
-      var note = event.detail;
-      resetWrapper();
-      drawNote(note);
-    });
+  var ContentManager = function () {
+    this._wrapper = document.querySelector('#note-content-wrapper');
   }
 
-  function resetWrapper() {
-    _wrapper.innerHTML = '';
+  ContentManager.prototype = {
+
+    resetWrapper(){
+      this._wrapper.innerHTML = '';
+    },
+
+    handleEvent(event){
+      switch (event.type) {
+        case 'note-open':
+          var note = event.detail;
+          this.resetWrapper();
+          this.drawNote(note);
+          break;
+      }
+    },
+
+    start() {
+      window.addEventListener('note-open', this);
+    },
+
+    drawNote(note) {
+      var title = note.title;
+      var h = document.createElement('h2');
+      h.textContent = title;
+      var passages = note.passages;
+      var buff = document.createDocumentFragment();
+      passages.forEach(function (passage) {
+        var p = document.createElement('p');
+        p.classList.add('note-passage');
+        p.textContent = passage;
+        buff.appendChild(p);
+      });
+      this._wrapper.appendChild(h);
+      this._wrapper.appendChild(buff);
+    }
+
   }
 
-  function drawNote(note) {
-    var title = note.title;
-    var h = document.createElement('h2');
-    h.textContent = title;
-    var passages = note.passages;
-    var buff = document.createDocumentFragment();
-    passages.forEach(function(passage) {
-      var p = document.createElement('p');
-      p.classList.add('note-passage');
-      p.textContent = passage;
-      buff.appendChild(p);
-    });
-    _wrapper.appendChild(h);
-    _wrapper.appendChild(buff);
-  }
+  exports.ContentManager = ContentManager;
 
-  document.addEventListener('DOMContentLoaded', function(event) {
-    start();
-  });
-})();
+})(window);
