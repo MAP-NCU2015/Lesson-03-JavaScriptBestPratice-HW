e92b00eebea404547b1e1e35d2b62d7cc62bc13e
Shark <sharknevercries@gmail.com>
2015-10-13 18:44:26 +0800

    add pure  test fucntion

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -2,8 +2,14 @@ describe('Test > ', function() {
   beforeEach(function() {
   });
 
-  it('My pure function', function() {
+  it('My pure function sin suc', function() {
     // Write any pure function assertion here.
-    assert.
+    assert.equal(Math.sin(Math.PI / 2), 1);
   });
+
+  it('My pure function sin failed', function () {
+    // Write any pure function assertion here.
+    assert.equal(Math.sin(Math.PI / 2), 0);
+  });
+
 });
