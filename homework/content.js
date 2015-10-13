e71468b20f9c9f82616cf19d39a7cfb4af2ba626
HanVincent <b84414@gmail.com>
2015-10-14 00:21:23 +0800

    Refer to example3. 1. Remove bind. 2. Try object properties w/ constructor & prototype.

--- homework/content.js
+++ homework/content.js
@@ -1,39 +1,37 @@
 'use strict';
 
-(function() {
-	//Get the id=note-content-wrapper
-  var _wrapper = document.querySelector('#note-content-wrapper');
+(function(exports) {
+    var TodoContentManager = {
+        _wrapper: null,
 
-  function start() {
-    window.addEventListener('note-open', (function(event) {
-      var note = event.detail;
-      resetWrapper();
-      drawNote(note);
-    }).bind(this));
-  }
+        resetWrapper() {
+                TodoContentManager._wrapper.innerHTML = '';
+            },
 
-  function resetWrapper() {
-    _wrapper.innerHTML = '';
-  }
+            drawNote(note) {
+                var title = note.title;
+                var h = document.createElement('h2');
+                h.textContent = title;
+                var passages = note.passages;
+                var buff = document.createDocumentFragment();
+                passages.forEach(function(passage) {
+                    var p = document.createElement('p');
+                    p.classList.add('note-passage');
+                    p.textContent = passage;
+                    buff.appendChild(p);
+                });
+                TodoContentManager._wrapper.appendChild(h);
+                TodoContentManager._wrapper.appendChild(buff);
+            },
 
-  function drawNote(note) {
-    var title = note.title;
-    var h = document.createElement('h2');
-    h.textContent = title;
-    var passages = note.passages;
-    var buff = document.createDocumentFragment();
-    passages.forEach(function(passage) {
-      var p = document.createElement('p');
-      p.classList.add('note-passage');
-      p.textContent = passage;
-      buff.appendChild(p);
-    });
-    _wrapper.appendChild(h);
-    _wrapper.appendChild(buff);
-  }
-
-	//先執行，當html載好後，執行後面的function。
-  document.addEventListener('DOMContentLoaded', (function(event) {
-    start();
-  }).bind(this));
-})();
+            init() {
+                TodoContentManager._wrapper = document.querySelector('#note-content-wrapper');
+                window.addEventListener('note-open', function(event) {
+                    var note = event.detail;
+                    TodoContentManager.resetWrapper();
+                    TodoContentManager.drawNote(note);
+                });
+            }
+    };
+    exports.TodoContentManager = TodoContentManager;
+})(window);
---
+++ homework/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  TodoListManager.init();
+  TodoContentManager.init();
+});
