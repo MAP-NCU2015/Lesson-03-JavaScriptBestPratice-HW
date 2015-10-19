777262bf9bc2ab8497b944f65cede5504c5cd63b
Meng-Gu Tsai <matt096374@gmail.com>
2015-10-19 21:09:49 +0800

    Update

--- homework/list.js
+++ homework/list.js
@@ -8,77 +8,77 @@
 
     ListNoteManager.prototype = {
         start() {
-                this._wrapper = document.querySelector('#note-list-wrapper');
-                this.fetchList().then((function(data) {
-                    this.updateList(data);
-                    this.drawList();
-                    this.preloadFirstNote();
-                }).bind(this));
-                window.addEventListener('click', (function(event) {
-                    this.onNoteOpen(event);
-                }).bind(this));
-            },
+            this._wrapper = document.querySelector('#note-list-wrapper');
+            this.fetchList().then((function(data) {
+                this.updateList(data);
+                this.drawList();
+                this.preloadFirstNote();
+            }).bind(this));
+            window.addEventListener('click', (function(event) {
+                this.onNoteOpen(event);
+            }).bind(this));
+        },
 
-            onNoteOpen(event) {
-                if (event.target.classList.contains('note-title')) {
-                    var id = event.target.dataset.noteId;
-                    var content = this._listNoteContent[id];
-                    window.dispatchEvent(new CustomEvent('note-open', {
-                        detail: content
-                    }));
-                };
-            },
+        onNoteOpen(event) {
+            if (event.target.classList.contains('note-title')) {
+                var id = event.target.dataset.noteId;
+                var content = this._listNoteContent[id];
+                window.dispatchEvent(new CustomEvent('note-open', {
+                    detail: content
+                }));
+            };
+        },
 
-            preloadFirstNote() {
-                if (this._listNoteContent.length !== 0) {
-                    var content = this._listNoteContent[0];
-                    window.dispatchEvent(new CustomEvent('note-open', {
-                        detail: content
-                    }));
-                }
-            },
-
-            updateList(list) {
-                this._listNoteContent = list;
-            },
-
-            drawList() {
-                var list = this._listNoteContent;
-                var ul = document.createElement('ul');
-                ul.id = 'note-title-list';
-                var buff = document.createDocumentFragment();
-                list.forEach(function(note, i) {
-                    var li = document.createElement('li');
-                    li.dataset.noteId = i;
-                    li.classList.add('note-title');
-                    li.textContent = note.title;
-                    // Note: buff is captured, so we now have a
-                    // little closure naturally.
-                    buff.appendChild(li);
-                });
-                ul.appendChild(buff);
-                this._wrapper.appendChild(ul);
-            },
-
-            fetchList() {
-                return new Promise((function(resolve, reject) {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', '/demo-list-notes.json', true);
-                    xhr.responseType = 'json';
-                    xhr.onreadystatechange = function(e) {
-                        // Watch out: we have a mysterious unknown 'this'.
-                        if (this.readyState === 4 && this.status === 200) {
-                            var listData = this.response;
-                            // The flow ends here.
-                            resolve(listData);
-                        } else if (this.status !== 200) {
-                            // Ignore error in this case.
-                            reject('FETCHING FAILED:' + this.status + ' ' + this.readyState);
-                        }
-                    }.bind(xhr);
-                    xhr.send();
+        preloadFirstNote() {
+            if (this._listNoteContent.length !== 0) {
+                var content = this._listNoteContent[0];
+                window.dispatchEvent(new CustomEvent('note-open', {
+                    detail: content
                 }));
             }
+        },
+
+        updateList(list) {
+            this._listNoteContent = list;
+        },
+
+        drawList() {
+            var list = this._listNoteContent;
+            var ul = document.createElement('ul');
+            ul.id = 'note-title-list';
+            var buff = document.createDocumentFragment();
+            list.forEach(function(note, i) {
+                var li = document.createElement('li');
+                li.dataset.noteId = i;
+                li.classList.add('note-title');
+                li.textContent = note.title;
+                // Note: buff is captured, so we now have a
+                // little closure naturally.
+                buff.appendChild(li);
+            });
+            ul.appendChild(buff);
+            this._wrapper.appendChild(ul);
+        },
+
+        fetchList() {
+            return new Promise((function(resolve, reject) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', '/demo-list-notes.json', true);
+                xhr.responseType = 'json';
+                xhr.onreadystatechange = function(e) {
+                    // Watch out: we have a mysterious unknown 'this'.
+                    if (this.readyState === 4 && this.status === 200) {
+                        var listData = this.response;
+                        // The flow ends here.
+                        resolve(listData);
+                    } else if (this.status !== 200) {
+                        // Ignore error in this case.
+                        reject('FETCHING FAILED:' + this.status + ' ' + this.readyState);
+                    }
+                }.bind(xhr);
+                xhr.send();
+            }));
+        }
     };
 
     exports.ListNoteManager = ListNoteManager;
