6fdd641d84a9298187b66d5cf5c7d4b694f587e1
jim83531 <jim83531@gmail.com>
2015-10-22 16:13:26 -0700

    Homework#3

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -1,8 +1,13 @@
 describe('Test > ', function() {
   beforeEach(function() {
+	  subject = new ContentManager();
   });
 
-  it('will test some pure functions', function() {
+  it(''Test resetWrapper function', function() {
     // Write any pure function assertion here.
+	subject._wrapper = document.createElement('div');
++        subject._wrapper.innerHTML = 'Test';
++        subject.resetWrapper();
++        assert.equal(subject._wrapper.innerHTML, '', "Failed~"
   });
 });
