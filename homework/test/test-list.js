d55e8150d616e3af8bc4a4c2188cebc7a3d78789
ZenithMeteor <zadays9057@gmail.com>
2015-10-23 13:30:08 +0800

    using test

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -1,9 +1,16 @@
 describe('Test > ', function() {
   beforeEach(function() {
+	subject = new TodoListManager();
   });
 
-  it('will test some pure functions', function() {
-    // Write any pure function assertion here.
-	
+  it('Can save some data', function(done) {
+    var dummyList = [
+      {checked: false, description: 'Dummy Todo #1'},
+      {checked: true, description: 'Dummy Todo #2'}
+    ];
+    subject.saveData(dummyList).then(function() {
+      assert.deepEqual(subject._listNoteContent, dummyList);
+      done();
+    }).catch(done);
   });
 });
