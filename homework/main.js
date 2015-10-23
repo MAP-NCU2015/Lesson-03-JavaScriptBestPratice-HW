0e8082fa44f52dbd2fedbcfeabe53fd2114e6b5f
lihungte96 <lihungte96@gmail.com>
2015-10-23 21:30:27 +0800

    step1 ...

---
+++ homework/main.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+// Kick off
+document.addEventListener('DOMContentLoaded', function (event) {
+    var todoListManager = new TodoListManager();
+    var todoInputManager = new TodoContentManager();
+    todoListManager.start();
+    TodoContentManager.start();
+});
