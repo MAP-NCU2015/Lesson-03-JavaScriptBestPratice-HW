ac3e7603d3ab1d2dcfd3eaa5d41be82db1fe3f60
jim83531 <jim83531@gmail.com>
2015-10-23 15:45:20 +0800

    Update test-list.js

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -6,8 +6,8 @@ describe('Test > ', function() {
   it(''Test resetWrapper function', function() {
     // Write any pure function assertion here.
 	subject._wrapper = document.createElement('div');
-+        subject._wrapper.innerHTML = 'Test';
-+        subject.resetWrapper();
-+        assert.equal(subject._wrapper.innerHTML, '', "Failed~"
+        subject._wrapper.innerHTML = 'Test';
+        subject.resetWrapper();
+        assert.equal(subject._wrapper.innerHTML, '', "Failed~"
   });
 });
