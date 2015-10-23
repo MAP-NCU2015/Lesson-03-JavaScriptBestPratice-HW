41101bca709101df9b0b040bf53efb8b17ddd44e
HanVincent <b84414@gmail.com>
2015-10-23 19:23:32 +0800

    Modify test-list.js

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -6,4 +6,12 @@
 	  it('will test sin', function() {
 		assert.equal(Math.sin(0), 0);
       });
+	  it('test function', function() {
+		var manager = new Manager();
+		assert.equal('2', manager.add(1, 1));
+	  })
     });
+	var Manager = function() {};
+	Manager.prototype.add = function(num1, num2){
+		return num1 + num2;
+	}
