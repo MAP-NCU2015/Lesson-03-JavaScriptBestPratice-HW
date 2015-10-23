c62d4cf0379764ba522793008cd7b41f49d97939
jim83531 <jim83531@gmail.com>
2015-10-23 16:05:22 +0800

    Update main.js

--- homework/main.js
+++ homework/main.js
@@ -1,9 +1,9 @@
 '+'use strict';
-+
-+  // Kick off
-+document.addEventListener('DOMContentLoaded', function(event) {
-+  var listNote = new ListNote();
-+  var contentManager = new ContentManager();
-+  listNote.start();
-+  contentManager.start();
-+});
+
+ // Kick off
+document.addEventListener('DOMContentLoaded', function(event) {
+  var listNote = new ListNote();
+  var contentManager = new ContentManager();
+  listNote.start();
+  contentManager.start();
+});
