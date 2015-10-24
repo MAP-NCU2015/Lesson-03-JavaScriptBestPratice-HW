119d2e76ee57a6e944386fde6f1e1e4534315f27
CaeserNieh <githubtryemail@gmail.com>
2015-10-24 13:01:50 +0800

    Deal with object properties w/ constructor & prototype

--- homework/content.js
+++ homework/content.js
@@ -1,37 +1,43 @@
 'use strict';
 
-(function() {
-  var _wrapper = document.querySelector('#note-content-wrapper');
+(function (exports){
 
-  function start() {
-    window.addEventListener('note-open', (function(event) {
-      var note = event.detail;
-      resetWrapper();
-      drawNote(note);
-    }).bind(this));
+  var ContentManager = function(){
+    this._wrapper = document.querySelector('#note-content-wrapper');
   }
 
-  function resetWrapper() {
-    _wrapper.innerHTML = '';
+  ContentManager.prototype = {
+    start() {
+        window.addEventListener('note-open', (function(event) {
+              var note = event.detail;
+              this.resetWrapper();
+              this.drawNote(note);
+          }).bind(this));
+    }, 
+    // original content.js start function
+    // call original contest.js resetWrapper function use "this"
+    // following are the same
+    resetWrapper(){
+      _wrapper.innerHTML = '';
+    },
+
+    drawNote(note){
+      var title = note.title;
+          var h = document.createElement('h2');
+          h.textContent = title;
+          var passages = note.passages;
+          var buff = document.createDocumentFragment();
+          passages.forEach(function(passage) {
+              var p = document.createElement('p');
+              p.classList.add('note-passage');
+              p.textContent = passage;
+              buff.appendChild(p);
+      });
+          this._wrapper.appendChild(h);
+          this._wrapper.appendChild(buff);
+    }
   }
 
-  function drawNote(note) {
-    var title = note.title;
-    var h = document.createElement('h2');
-    h.textContent = title;
-    var passages = note.passages;
-    var buff = document.createDocumentFragment();
-    passages.forEach(function(passage) {
-      var p = document.createElement('p');
-      p.classList.add('note-passage');
-      p.textContent = passage;
-      buff.appendChild(p);
-    });
-    _wrapper.appendChild(h);
-    _wrapper.appendChild(buff);
-  }
+  exports.ContentManager = ContentManager;
 
-  document.addEventListener('DOMContentLoaded', (function(event) {
-    start();
-  }).bind(this));
-})();
+})(window);
