8725f4c57eff28ad64730ff8057d4e11b65db81a
CrashedBboy <crashedbboy@gmail.com>
2015-10-25 21:12:58 +0800

    Using closure for class

--- homework/content.js
+++ homework/content.js
@@ -1,39 +1,41 @@
 'use strict';
+(function(exports){
+	var ContentManager = function() {
+		this._wrapper = document.querySelector('#note-content-wrapper');
+		document.addEventListener('DOMContentLoaded', this.start.bind(this));
+	}
 
-function Note() {
-  this._wrapper = document.querySelector('#note-content-wrapper');
-  document.addEventListener('DOMContentLoaded', this.start.bind(this));
-}
+	ContentManager.prototype = {
+		start() {
+			window.addEventListener('note-open', this.updateContent.bind(this));
+		},
 
-Note.prototype.start = function() {
-	// 'note-open' will be triggered at list.js
-	window.addEventListener('note-open', this.updateContent.bind(this));
-}
+		updateContent(event) {
+			var note = event.detail;
+			this.resetWrapper();
+			this.drawNote(note);
+		},
 
-Note.prototype.updateContent = function(event) {
-	var note = event.detail;
-	this.resetWrapper();
-	this.drawNote(note);
-}
+		resetWrapper() {
+			this._wrapper.innerHTML = '';
+		},
 
-Note.prototype.resetWrapper = function() {
-	this._wrapper.innerHTML = '';
-}
+		drawNote(note) {
+			var title = note.title;
+			var h = document.createElement('h2');
+			h.textContent = title;
+			var passages = note.passages;
+			var buff = document.createDocumentFragment();
+			passages.forEach(function(passage) {
+				var p = document.createElement('p');
+				p.classList.add('note-passage');
+				p.textContent = passage;
+				buff.appendChild(p);
+			});
+			this._wrapper.appendChild(h);
+			this._wrapper.appendChild(buff);
+		}
+	}
 
-Note.prototype.drawNote = function(note) {
-	var title = note.title;
-	var h = document.createElement('h2');
-	h.textContent = title;
-	var passages = note.passages;
-	var buff = document.createDocumentFragment();
-	passages.forEach(function(passage) {
-		var p = document.createElement('p');
-		p.classList.add('note-passage');
-		p.textContent = passage;
-		buff.appendChild(p);
-	});
-	this._wrapper.appendChild(h);
-	this._wrapper.appendChild(buff);
-}
-
-var note = new Note();
+	window.ContentManager = ContentManager;
+})(window);
--- homework/list.js
+++ homework/list.js
@@ -1,25 +1,63 @@
 'use strict';
+(function(exports) {
+	var ListManager = function() {
+		this._listNoteContent = [];
+		this._wrapper = document.querySelector('#note-list-wrapper');
+		document.addEventListener('DOMContentLoaded', this.start.bind(this));
+	}
 
-function List() {
-  this._listNoteContent = [];
-  this._wrapper = document.querySelector('#note-list-wrapper');
-	document.addEventListener('DOMContentLoaded', this.start.bind(this));
-}
+	ListManager.prototype = {
+		start() {
+			this.fetchList(this.afterFetch.bind(this));
+			window.addEventListener('click', this.onNoteOpen.bind(this));
+		},
+		fetchList(afterFetch) {
+			var url = 'http://127.0.0.1:8000/demo-list-notes.json';
+			sendXHR(url).then( function onFulfilled(value) {
+				afterFetch(value);
+			}).catch( function onRejected(error) {
+				console.error(error);
+			})
+		},
+		afterFetch(data) {
+			this.updateList(data);
+			this.drawList();
+			this.preloadFirstNote();
+		},
 
-List.prototype.start = function() {
-	this.fetchList(this.afterFetch.bind(this));
-	window.addEventListener('click', this.onNoteOpen.bind(this));
-}
+		updateList(list) {
+			this._listNoteContent = list;
+		},
 
-/** Need to use Promise*/
-List.prototype.fetchList = function(afterFetch) {
-	var url = 'http://140.115.50.54:8000/demo-list-notes.json';
-	sendXHR(url).then( function onFulfilled(value) {
-		afterFetch(value);
-	}).catch( function onRejected(error) {
-		console.error(error);
-	})
-}
+		drawList() {
+			var list = this._listNoteContent;
+			var ul = document.createElement('ul');
+			ul.id = 'note-title-list';
+			var buff = document.createDocumentFragment();
+			list.forEach(insertItem.bind(buff));
+			ul.appendChild(buff);
+			this._wrapper.appendChild(ul);
+		},
+
+		preloadFirstNote() {
+			if (this._listNoteContent.length !== 0) {
+				var content = this._listNoteContent[0];
+				window.dispatchEvent(new CustomEvent('note-open',
+					{ detail: content }));
+			}
+		},
+
+		onNoteOpen(event) {
+			if (event.target.classList.contains('note-title')) {
+				var id = event.target.dataset.noteId;
+				var content = this._listNoteContent[id];
+				window.dispatchEvent(new CustomEvent('note-open',
+					{ detail: content }));
+			};
+		}
+	}
+	exports.ListManager = ListManager;
+})(window);
 
 function sendXHR(url) {
 	return  new Promise(function(resolve, reject) {
@@ -37,43 +75,6 @@ function sendXHR(url) {
 	});
 }
 
-List.prototype.afterFetch = function(data) {
-	this.updateList(data);
-	this.drawList();
-	this.preloadFirstNote();
-}
-
-List.prototype.updateList = function(list) {
-	this._listNoteContent = list;
-}
-
-List.prototype.drawList = function() {
-	var list = this._listNoteContent;
-	var ul = document.createElement('ul');
-	ul.id = 'note-title-list';
-	var buff = document.createDocumentFragment();
-	list.forEach(insertItem.bind(buff));
-	ul.appendChild(buff);
-	this._wrapper.appendChild(ul);
-}
-
-List.prototype.preloadFirstNote = function() {
-	if (this._listNoteContent.length !== 0) {
-		var content = this._listNoteContent[0];
-		window.dispatchEvent(new CustomEvent('note-open',
-			{ detail: content }));
-	}
-}
-
-List.prototype.onNoteOpen = function(event) {
-	if (event.target.classList.contains('note-title')) {
-		var id = event.target.dataset.noteId;
-		var content = this._listNoteContent[id];
-		window.dispatchEvent(new CustomEvent('note-open',
-			{ detail: content }));
-	};
-}
-
 function insertItem(element, index) {
 	var li = document.createElement('li');
 	li.dataset.noteId = index;
@@ -81,5 +82,3 @@ function insertItem(element, index) {
 	li.textContent = element.title;
 	this.appendChild(li);
 }
-
-var list = new List();
