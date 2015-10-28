534a70484d0f6f85b9860c9d7b9bb89a6e75c195
BDSword <thetruebloodsword@gmail.com>
2015-10-28 11:26:14 +0800

    Write meaningful comments in JSDoc format

--- homework/content.js
+++ homework/content.js
@@ -1,11 +1,28 @@
 'use strict';
 
+/**
+ * @typedef {Object} Note
+ * @property {String} title
+ * @property {String} passages
+ */
+
 (function(exports) {
 
+  /**
+   * Create a instance of ContentManager
+   *
+   * @constructor
+   * @this {ContentManager}
+   */
   var ContentManager = function () {
     this._wrapper = document.querySelector('#note-content-wrapper');
   }
 
+  /**
+   * Add an event handler for note-open event which draw correct note contents
+   *
+   * @this {ContentManager}
+   */
   ContentManager.prototype.start = function () {
     window.addEventListener('note-open', (function(event) {
       var note = event.detail;
@@ -14,10 +31,21 @@
     }).bind(this));
   }
 
+  /**
+   * Clear the content(HTML) in _wrapper
+   *
+   * @this {ContentManager}
+   */
   ContentManager.prototype.resetWrapper = function () {
     this._wrapper.innerHTML = '';
   }
 
+  /**
+   * Draw the given note in the content wrapper
+   *
+   * @this   {ContentManager}
+   * @param  {Note} Note to draw
+   */
   ContentManager.prototype.drawNote = function (note) {
     var title = note.title;
     var h = document.createElement('h2');
--- homework/list.js
+++ homework/list.js
@@ -1,11 +1,30 @@
 'use strict';
 
+/**
+ * @typedef {Object} Note
+ * @property {String} title
+ * @property {String} passages
+ */
+
 (function(exports) {
+
+  /**
+   * Create a instance of ListManager
+   * @constructor
+   * @this {ListManager}
+   */
   var ListManager = function(){
       this._wrapper = document.querySelector('#note-list-wrapper');
       this._listNoteContent = [];
   }
 
+
+  /**
+   * Fetch the notes from remote server, draw them in the notes wrapper,
+   * and bind click event to dispatch note-open event
+   *
+   * @this {ListManager}
+   */
   ListManager.prototype.start = function(){
       this.fetchList().then((function(data) {
         this.updateList(data);
@@ -19,6 +38,12 @@
       }).bind(this));
   };
 
+
+  /**
+   * Dispatch the note-open event and prepare the param for the dispatched event
+   *
+   * @this {ListManager}
+   */
   ListManager.prototype.onNoteOpen = function(){
       if (event.target.classList.contains('note-title')) {
         var id = event.target.dataset.noteId;
@@ -28,6 +53,12 @@
       };
   },
 
+
+  /**
+   * Preload the first note at the first time this instance start
+   *
+   * @this {ListManager}
+   */
   ListManager.prototype.preloadFirstNote = function() {
     if (this._listNoteContent.length !== 0) {
       var content = this._listNoteContent[0];
@@ -36,10 +67,22 @@
     }
   },
 
+
+  /**
+   * Update the list in this ListManager instance
+   *
+   * @this {ListManager}
+   */
   ListManager.prototype.updateList = function(list) {
     this._listNoteContent = list;
   },
 
+
+  /**
+   * Draw the note list in the notes wrapper
+   *
+   * @this {ListManager}
+   */
   ListManager.prototype.drawList = function() {
     var list = this._listNoteContent;
     var ul = document.createElement('ul');
@@ -58,6 +101,15 @@
     this._wrapper.appendChild(ul);
   },
 
+
+  /**
+   * Fetch the notes list from remote server using ajax
+   *
+   * @this     {ListManager}
+   * @return   {Promise} Result
+   * @resolves {Note[]} A list of note
+   * @reject   {XMLHttpRequest} The XMLHttpRequest instance
+   */
   ListManager.prototype.fetchList = function() {
     return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
@@ -78,9 +130,18 @@
     });
   }
 
+
+  /**
+   * Calculate the degree in Fahrenheit scale for a given Celsius scale degree
+   *
+   * @param  {Float} A degree in Celsius scale
+   * @this   {ListManager}
+   * @return {Float} A degree in Fahrenheit scale
+   */
   ListManager.prototype.getFahrenheit = function (celsius) {
     return celsius * 1.8000 + 32.0000;
   }
+
 
   exports.ListManager = ListManager;
 })(window);
