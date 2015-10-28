3e7672289d3dca5ec3ef14bda7e4b31a468b5ffe
kaofushwai <panhsuanjen@gmail.com>
2015-10-28 23:52:55 +0800

    Deal with asynchronous flows with Promise

--- homework/list.js
+++ homework/list.js
@@ -2,80 +2,82 @@
 
 (function(exports) {
 
-  var ListManager = function () {
-  this._listNoteContent = [];
-  this._wrapper = document.querySelector('#note-list-wrapper');
+	var ListManager = function();
+	 this._listNoteContent = [];
+	 this._wrapper = document.querySelector('#note-list-wrapper');
   }
 
   ListManager.prototype = {
-   start() {
-   this.fetchList((function(data) {
-   this.updateList(data);
-   this.drawList();
-   this.preloadFirstNote();
-   }).bind(this));
-   window.addEventListener('click', (function(event) {
-    this.onNoteOpen(event);
-	}).bind(this));
- },
-
-  onNoteOpen(event) {
-     if (event.target.classList.contains('note-title')) {
-      var id = event.target.dataset.noteId;
-	  var content = this._listNoteContent[id];
-      window.dispatchEvent(new CustomEvent('note-open',
-        { detail: content }));
-         };
-    },
-
-  preloadFirstNote() {
-     if (this._listNoteContent.length !== 0) {
-       var content = this._listNoteContent[0];
-       window.dispatchEvent(new CustomEvent('note-open',
-         { detail: content }));
-     }
-   },
- 
-  updateList(list) {
-     this._listNoteContent = list;
-    },
- 
-   drawList() {
-      var list = this._listNoteContent;
-      var ul = document.createElement('ul');
-      ul.id = 'note-title-list';
-      var buff = document.createDocumentFragment();
-      list.forEach(function(note, i) {
-        var li = document.createElement('li');
-        li.dataset.noteId = i;
-        li.classList.add('note-title');
-        li.textContent = note.title;
-
-        buff.appendChild(li);
-      });
-      ul.appendChild(buff);
-      this._wrapper.appendChild(ul);
-    },
-
-    fetchList(afterFetch) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'http://127.0.0.1:8000/demo-list-notes.json', true);
-      xhr.responseType = 'json';
-      xhr.onreadystatechange =  (function(e) {
-        // Watch out: we have a mysterious unknown 'this'.
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          var listData = xhr.response;
-          // The flow ends here.
-          afterFetch(listData);
-        } else if (xhr.status !== 200 ){
-          // Ignore error in this case.
-        }
-      }).bind(this);
-      xhr.send();
-    }
+  start(){
+  this.fetchList()
+  .then((function)(data){
+  this.updateList(data);
+  this.drawList();
+  this.preloadFirstNote();
+  }).bind(this))
+  .catch((function(){
   
+  }).bind(this) );
+  
+  window.addEventListener('click',(function(event){
+  this.onNote(event);
+  }).bind(this))
+  },
+
+  onNoteOpen(event){
+   if(event.target.classList,contains('note-title')){
+   var id = event.target.dataset.noteId;
+   vat content = this._listNoteContent[id];
+   window.dispatchEvent(new CustomEvent('note-open',
+	detail: content}));
+	};
+ },
+  
+  preloadFirstNote(){
+	if(this.listNoteContent.lenght !== 0){
+	var content = this._listNoteContent[0];
+	window.dispatchEvent(new CustomEvent('note-open',
+		{detail: content}));
+	}
+  },
+  
+  updateList(List){
+	this.listNoteContent = List;
+  },
+
+  drawList(){
+  var List = this._listNoteContent;
+  var ul = document.createElement('ul');
+  ul.id = 'note=title-list';
+  var buff = document.createDocumentFragment();
+  list.forEach(function(note, i ){
+  var li  = document.createElement('li');
+  li.dataset.noteId = i;
+  li.classList.add('note-title');
+  li.textContent = note.title;
+  buff.appendChild(li);
+  });
+  ul.appendChild(buff);
+  this._wrapper.appendChild(ul);
+  },
+  
+  fetchList(){
+	return new Promise(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET','http://127.0.1:8000/demo-list-notes.json',true);
+		xhr.responseType = 'json';
+		xhr.onreadystatechange=function(e){
+			if(xhr.readyState==&& xhr.status==200){
+			resolve(xhr.response);
+			}else if (xhr.status !=200){
+			reject(xhr);
+			}
+		};
+		xhr.send();
+		});
+	}
   }
-   exports.ListManager = ListManager
-   })(window);
+  
 
-
+  exports.ListManager = ListManager
+})(window);
