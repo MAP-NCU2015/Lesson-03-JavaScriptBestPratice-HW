2efc14989acba18de06da286f01ce082ca0b9953
Anchorboy <s810142000@gmail.com>
2015-10-28 19:27:55 +0800

    Deal with object properties w/ constructor & prototype

--- homework/content.js
+++ homework/content.js
@@ -1,37 +1,44 @@
 'use strict';
 
 (function() {
-  var _wrapper = document.querySelector('#note-content-wrapper');
 
-  function start() {
-    window.addEventListener('note-open', function(event) {
-      var note = event.detail;
-      resetWrapper();
-      drawNote(note);
+  var ContentManager = function () {
+    var _wrapper = document.querySelector('#note-content-wrapper'); 
+  }
+ 
+  ContentManager.prototype = {
+
+    start() {
+      window.addEventListener('note-open', function(event) {
+        var note = event.detail;
+        resetWrapper();
+        drawNote(note);
+      });
+    },
+
+    resetWrapper() {
+      _wrapper.innerHTML = '';
+    },
+
+    drawNote(note) {
+      var title = note.title;
+      var h = document.createElement('h2');
+      h.textContent = title;
+      var passages = note.passages;
+      var buff = document.createDocumentFragment();
+      passages.forEach(function(passage) {
+        var p = document.createElement('p');
+        p.classList.add('note-passage');
+        p.textContent = passage;
+        buff.appendChild(p);
+      });
+      _wrapper.appendChild(h);
+      _wrapper.appendChild(buff);
+    },
+
+    document.addEventListener('DOMContentLoaded', function(event) {
+      start();
     });
-  }
 
-  function resetWrapper() {
-    _wrapper.innerHTML = '';
   }
-
-  function drawNote(note) {
-    var title = note.title;
-    var h = document.createElement('h2');
-    h.textContent = title;
-    var passages = note.passages;
-    var buff = document.createDocumentFragment();
-    passages.forEach(function(passage) {
-      var p = document.createElement('p');
-      p.classList.add('note-passage');
-      p.textContent = passage;
-      buff.appendChild(p);
-    });
-    _wrapper.appendChild(h);
-    _wrapper.appendChild(buff);
-  }
-
-  document.addEventListener('DOMContentLoaded', function(event) {
-    start();
-  });
-})();
+})(window);
