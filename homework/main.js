08d925684c1a1ca73f1372b517f8c24f5504aed8
Davisanity <davidlin12tw@gmail.com>
2015-10-29 02:29:25 +0800

    Create main.js

---
+++ homework/main.js
@@ -0,0 +1,8 @@
+'use strict';
+// Kick off
+document.addEventListener('DOMContentLoaded', function(event) {
+    var listNoteManager = new ListNoteManager();
+    var listNoteContentManager = new ListNoteContentManager();
+    listNoteContentManager.start();
+    listNoteManager.start();
+});
