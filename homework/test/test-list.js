53923a4526897eff9253237183e843393fadbff2
nickyeh97 <nickyeh97@gmail.com>
2015-10-29 15:56:47 +0800

    My first test case

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -1,8 +1,12 @@
-describe('Test > ', function() {
+describe('Test ToDoListManager ', function() {
+  var subject;
   beforeEach(function() {
+      subject = new ToDoListManager();
   });
 
   it('will test some pure functions', function() {
-    // Write any pure function assertion here.
+    var test = subject;
+    var sum = test.puerFunction(1,1);
+    assert.equal(sum,2,'It is equal to 2!');
   });
 });
