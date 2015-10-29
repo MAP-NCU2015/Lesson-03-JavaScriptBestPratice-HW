909482f17ed1ce4242317d2c22ae82bbd9da7722
Davisanity <davidlin12tw@gmail.com>
2015-10-29 17:33:37 +0800

    Update test-list.js

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -1,8 +1,13 @@
-describe('Test > ', function() {
-  beforeEach(function() {
-  });
-
-  it('will test some pure functions', function() {
-    // Write any pure function assertion here.
-  });
+describe('Test ListNoteContentManager', function() {
+    var subject;
+    beforeEach(function() {
+        subject = new ListNoteContentManager();
+    });
+it('Test resetWrapper function', function() {
+        // Write any pure function assertion here.
+        subject._wrapper = document.createElement('div');
+        subject._wrapper.innerHTML = 'Test';
+        subject.resetWrapper();
+        assert.equal(subject._wrapper.innerHTML, '', "Reset Failed!");
+    });
 });
