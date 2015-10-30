d203235a577ef7a2a661b9948c6498699b89146c
DannyJJJJ <danny89019@gmail.com>
2015-10-30 12:09:31 +0800

    Update test-list.js

--- homework/test/test-list.js
+++ homework/test/test-list.js
@@ -10,4 +10,8 @@ describe('Test > ', function() {
     var sum = test.puerFunction(1,4);
     assert.equal(sum,5,'It is equal to 5!');
   });
+  
+  Manager.prototype.puerFunction(a,b){
+    return a+b;
+  }
 });
