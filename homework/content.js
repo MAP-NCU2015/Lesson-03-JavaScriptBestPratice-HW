04c11feb950738fdb30e94a6ed16c007f1f022b7
DannyJJJJ <danny89019@gmail.com>
2015-10-30 11:14:33 +0800

    Update content.js
    First change according to teacher's example 1 to exampe 5

--- homework/content.js
+++ homework/content.js
@@ -1,21 +1,29 @@
 'use strict';
 
-(function() {
-  var _wrapper = document.querySelector('#note-content-wrapper');
+(function(exports) {
+  
+  var TodoContentManager = function() {
+  // Local data storage; should sync up with the server.
+  this._listTodoItem = [];
+  // Will be an element as the list wrapper.
+  this._wrapper = document.querySelector('#note-content-wrapper');
+};
 
-  function start() {
+TodoContentManager.prototype = {
+  
+  start() {
     window.addEventListener('note-open', function(event) {
       var note = event.detail;
       resetWrapper();
       drawNote(note);
-    });
-  }
+    }).bind(this);
+  },
 
-  function resetWrapper() {
-    _wrapper.innerHTML = '';
-  }
+  resetWrapper() {
+    this._wrapper.innerHTML = '';
+  },
 
-  function drawNote(note) {
+  drawNote(note) {
     var title = note.title;
     var h = document.createElement('h2');
     h.textContent = title;
@@ -27,11 +35,11 @@
       p.textContent = passage;
       buff.appendChild(p);
     });
-    _wrapper.appendChild(h);
-    _wrapper.appendChild(buff);
+    this._wrapper.appendChild(h);
+    this._wrapper.appendChild(buff);
   }
+};
 
-  document.addEventListener('DOMContentLoaded', function(event) {
-    start();
-  });
-})();
+  
+  exports.TodoContentManager = TodoListManager;
+})(window);
